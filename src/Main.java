febb6e3bfbd21523db29b2a88df9488464680ba9
Teng Yao Long <87478734+tengyaolong2000@users.noreply.github.com>
2021-12-09 14:05:32 +0800

    Initial commit!!!

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String t = "";
+        System.out.println(t);
+        t = t + "Joe...";
+        System.out.println(t);
+        t = t + " went fishing";
+        System.out.println(t);
+
+        float f = 51.49f;
+        System.out.println(f);
+        int i = Math.round(f);
+        System.out.println(i);
+
+        //equation to solve hypotenuse
+        double a = 3;
+        double b = 4;
+        double h = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+        System.out.println(h); //print out the results
+    }
+}
